cb646b712a0814818ea2d575defda6b8fab1dcfe
sanuv9683 <sanuv9683@gmail.com>
2022-05-12 16:05:23 +0530

    Content Updatefd

--- Layered_Architecture/src/dao/Test.java
+++ Layered_Architecture/src/dao/Test.java
@@ -6,13 +6,16 @@ package dao;
  **/
 public class Test {
     public static void main(String[] args) {
-   /*     DAOFactory daoFactory = new DAOFactory();
-        DAOFactory daoFactor2 = new DAOFactory();
-        DAOFactory daoFactor3 = new DAOFactory();
-        DAOFactory daoFactor4 = new DAOFactory();*/
+//        DAOFactory daoFactory = new DAOFactory();
+//        DAOFactory daoFactor2 = new DAOFactory();
 
-        DAOFactory daoFactory = DAOFactory.getDaoFactory();
-        daoFactory.test();
+//        System.out.println(daoFactory);
+//        System.out.println(daoFactor2);
+
+        DAOFactory daoFactory1 = DAOFactory.getDaoFactory();
+        DAOFactory daoFactory2 = DAOFactory.getDaoFactory();
+        System.out.println(daoFactory1);
+        System.out.println(daoFactory2);
 
 
     }
