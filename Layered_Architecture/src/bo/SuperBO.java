5c027ec1dff3c5c06766dec7fd4e9f5fb60e6be3
sanuv9683 <sanuv9683@gmail.com>
2022-05-19 11:39:12 +0530

    Packaging convention aded

--- Layered_Architecture/src/bo/SuperBO.java
+++
@@ -1,8 +0,0 @@
-package bo;
-
-/**
- * @author : Sanu Vithanage
- * @since : 0.1.0
- **/
-public interface SuperBO {
-}
---
+++ Layered_Architecture/src/lk/ijse/pos/bo/SuperBO.java
@@ -0,0 +1,8 @@
+package lk.ijse.pos.bo;
+
+/**
+ * @author : Sanu Vithanage
+ * @since : 0.1.0
+ **/
+public interface SuperBO {
+}
